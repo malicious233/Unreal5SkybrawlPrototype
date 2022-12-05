cc04b72b1760d335766523ddeb4c682f5560c9a6
malicious233 <alfons.hedstrom@gmail.com>
2022-12-05 16:33:28 +0100

    * Implemented knockback mechanic for hitboxes, the knockback direction is pulled from the AttackData * AerialChop has an early and let hitbox, the early hitbox has silly vertical knockback

---
+++ Source/SkybrawlProject2022/CharacterScripts/SB_KnockbackInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SB_KnockbackInterface.h"
+
+
+// Add default functionality here for any ISB_KnockbackInterface functions that are not pure virtual.
---
+++ Source/SkybrawlProject2022/CharacterScripts/SB_KnockbackInterface.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SB_KnockbackInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class USB_KnockbackInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SKYBRAWLPROJECT2022_API ISB_KnockbackInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
