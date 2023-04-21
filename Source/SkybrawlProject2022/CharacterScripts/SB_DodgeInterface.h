e507239b4e6662d229be6e7031f2b2bf6916ace9
malicious233 <alfons.hedstrom@gmail.com>
2023-04-21 16:31:51 +0200

    * Played with some VFX with refraction * Added interface for dodging (not implementd yet)

---
+++ Source/SkybrawlProject2022/CharacterScripts/SB_DodgeInterface.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SB_DodgeInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class USB_DodgeInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SKYBRAWLPROJECT2022_API ISB_DodgeInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "Combat")
+	void Dodge();
+};
